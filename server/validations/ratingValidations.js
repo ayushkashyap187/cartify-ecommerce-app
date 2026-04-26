5a1245dddddd58a6555ea0256ccc7e3bd10d9bd4
ayushkashyap187 <ayushkashyap82075@gmail.com>
2026-04-26 17:47:27 +0530

    Delete server directory

--- server/validations/ratingValidations.js
+++
@@ -1,9 +0,0 @@
-const { body } = require("express-validator");
-module.exports = [
-  body("rating")
-    .not()
-    .isEmpty()
-    .trim()
-    .escape()
-    .withMessage("rating is required"),
-];
